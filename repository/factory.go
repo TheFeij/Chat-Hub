319d318ada4434fe55602cf7ff87b33467fdba9d
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-30 21:51:23 +0330

    renamed sql package to postgres

--- repository/factory.go
+++ repository/factory.go
@@ -1,12 +1,12 @@
 package repository
 
-import "Chat-Server/repository/db/sql"
+import "Chat-Server/repository/db/postgres"
 
 // NewRepository returns a new repository with the given type
 func NewRepository(databaseType DatabaseType) Repository {
 	switch databaseType {
 	case Postgres:
-		return sql.NewPostgresRepository()
+		return postgres.NewPostgresRepository()
 	}
 
 	return nil
