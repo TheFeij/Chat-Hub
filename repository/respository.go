5ddac4decd98001da4aba58d8ae08ee155a486bb
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-30 23:01:34 +0330

    changed methods to get and return pointers to types

--- repository/respository.go
+++ repository/respository.go
@@ -7,8 +7,8 @@ import (
 // Repository implements the required methods for the business layer to interact with the data layer
 type Repository interface {
 	// AddMessage adds a message to the data layer
-	AddMessage(message io.Message) (io.Message, error)
+	AddMessage(message *io.Message) (*io.Message, error)
 
 	// GetAllMessages retrieves all messages from the database
-	GetAllMessages() ([]io.Message, error)
+	GetAllMessages() ([]*io.Message, error)
 }
