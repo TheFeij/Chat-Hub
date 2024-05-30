9fbe8839a709c75390ddf7ad4e9ffe5c5d275754
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-30 21:36:55 +0330

    defined Message type

---
+++ repository/io/types.go
@@ -0,0 +1,9 @@
+package io
+
+// The Message represents a repository message
+type Message struct {
+	// Text of the message
+	Text string
+	// Author of the message (username of the person who sent the message)
+	Author string
+}
