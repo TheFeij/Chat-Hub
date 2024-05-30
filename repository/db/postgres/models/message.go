3d7f3065e6847284d0467a06c1e9f42f2bf059f2
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-30 23:00:50 +0330

    added gorm belongs to association to User model

--- repository/db/postgres/models/message.go
+++ repository/db/postgres/models/message.go
@@ -5,5 +5,5 @@ type Message struct {
 	ID     uint   `gorm:"column:id;primaryKey"`
 	Author string `gorm:"column:author;not null"`
 	Text   string `gorm:"column:tex;max:1024;min=1;not null"`
-	User   User   `gorm:"foreignKey:username;references:author"`
+	User   User   `gorm:"foreignKey:Author;references:Username"`
 }
