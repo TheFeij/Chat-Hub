4493a17435482ac141e83889a8cb4a67f331a211
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-30 23:34:38 +0330

    deleted

--- repository/io/types.go
+++
@@ -1,9 +0,0 @@
-package io
-
-// The Message represents a repository message
-type Message struct {
-	// Text of the message
-	Text string
-	// Author of the message (username of the person who sent the message)
-	Author string
-}
