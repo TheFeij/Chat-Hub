718f03fb0c756a393ce368574c1021a44bd32f53
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-30 21:08:04 +0330

    defined Session model

---
+++ repository/db/sql/models/session.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+// Session represents a user session
+type Session struct {
+	ID           uint           `gorm:"column:id;primaryKey"`
+	Username     string         `gorm:"column:username;not null"`
+	RefreshToken string         `gorm:"column:refresh_token;not null"`
+	UserAgent    string         `gorm:"column:user_agent;not null"`
+	ClientIP     string         `gorm:"column:client_ip;not null"`
+	IsBlocked    bool           `gorm:"column:is_blocked;default:false;not null"`
+	CreatedAt    time.Time      `gorm:"column:created_at;not null"`
+	ExpiresAt    time.Time      `gorm:"column:expires_at;not null"`
+	DeletedAt    gorm.DeletedAt `gorm:"column:deleted_at"`
+	User         User           `gorm:"foreignKey:username;references:username"`
+}
