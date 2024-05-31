d75b70ddb30b811cf7f48c1abe80e67bd96ea669
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-31 12:06:00 +0330

    added unit test for GetConfig function

--- config/config_test.go
+++ config/config_test.go
@@ -1,3 +1,20 @@
 package config
 
-// TODO: add config tests here
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+// TestGetConfig tests GetConfig function
+func TestGetConfig(t *testing.T) {
+	conf := GetConfig("config_test", "json", ".")
+	require.NotEmpty(t, conf)
+
+	require.Equal(t, "SERVER_ADDRESS", conf.serverAddress)
+	require.Equal(t, "DATABASE_ADDRESS", conf.databaseAddress)
+	require.Equal(t, "TEST_DATABASE_ADDRESS", conf.testDatabaseAddress)
+	require.Equal(t, "TOKEN_SYMMETRIC_KEY", conf.tokenSymmetricKey)
+	require.Equal(t, 15*time.Minute, conf.accessTokenDuration)
+	require.Equal(t, 24*time.Hour, conf.refreshTokenDuration)
+}
