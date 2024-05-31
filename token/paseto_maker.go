396a8ef45c087e31f3cd7628c6ef3c67a37652ae
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-31 11:24:36 +0330

    defined PasetoMaker implementation of the Maker interface

---
+++ token/paseto_maker.go
@@ -0,0 +1,54 @@
+package token
+
+import (
+	"fmt"
+	"github.com/o1egl/paseto"
+	"golang.org/x/crypto/chacha20poly1305"
+	"time"
+)
+
+// PasetoMaker implements Maker interface, creates and verifies paseto tokens
+type PasetoMaker struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+// NewPasetoMaker creates a new PasetoMaker instance and returns it as Maker
+func NewPasetoMaker(symmetricKey string) (Maker, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters\n", chacha20poly1305.KeySize)
+	}
+
+	return &PasetoMaker{
+		paseto:       paseto.NewV2(),
+		symmetricKey: []byte(symmetricKey),
+	}, nil
+}
+
+// CreateToken creates adn returns a token
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", nil, err
+	}
+
+	pasetoToken, err := maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+	return pasetoToken, payload, err
+}
+
+// VerifyToken verifies the input token and if valid, returns the payload
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, ErrExpiredToken
+	}
+
+	return payload, nil
+}
