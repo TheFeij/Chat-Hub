0fcee6dbc0cae93d79efcb5707127d95c838d71a
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-31 11:24:00 +0330

    defined Payload struct, NewPayload functions and IsValid method

---
+++ token/payload.go
@@ -0,0 +1,44 @@
+package token
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+// possible errors returned by this package
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("token is invalid")
+)
+
+// Payload represent a token payload
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// NewPayload creates and returns a new payload
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}, nil
+}
+
+// Valid checks if the token is valid or not
+func (p *Payload) Valid() error {
+	if time.Now().After(p.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
