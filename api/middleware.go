2aae33788fe07674f86eebc429d4988edaa609b5
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-06-03 13:34:11 +0330

    changed authMiddleware to get access token from cookies

--- api/middleware.go
+++ api/middleware.go
@@ -5,41 +5,25 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"strings"
 )
 
 const (
-	authorizationHeaderKey  string = "authorization"
+	authorizationCookieName string = "accessToken"
 	authorizationPayloadKey string = "authorization_payload"
-	authorizationTypeBearer string = "bearer"
 )
 
-// authMiddleware checks for authorization header and if valid, extracts the token payload
+// authMiddleware checks for access token in the cookies and if valid, extracts the token payload
 // and saves it as authorizationPayloadKey in the context
 func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
 	return func(context *gin.Context) {
-		authorizationHeader := context.GetHeader(authorizationHeaderKey)
-		if len(authorizationHeader) == 0 {
-			err := fmt.Errorf("athorization header is not provided")
+		accessToken, err := context.Cookie(authorizationCookieName)
+
+		if err != nil {
+			err := fmt.Errorf("access token not provided")
 			context.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
 			return
 		}
 
-		fields := strings.Fields(authorizationHeader)
-		if len(fields) < 2 {
-			err := fmt.Errorf("invalid authorization format")
-			context.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
-			return
-		}
-
-		authorizationType := fields[0]
-		if strings.ToLower(authorizationType) != authorizationTypeBearer {
-			err := fmt.Errorf("invalid authorization header format")
-			context.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
-			return
-		}
-
-		accessToken := fields[1]
 		payload, err := tokenMaker.VerifyToken(accessToken)
 		if err != nil {
 			context.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
