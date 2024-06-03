65f9ebce65042a109af69a24dfbebe6e00c0d01d
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-06-03 13:33:11 +0330

    added logic to start the server

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"Chat-Server/api"
+	"Chat-Server/config"
+	"Chat-Server/repository/db/postgres"
+	"Chat-Server/token"
+)
+
 func main() {
-	// TODO start the application
+	configs := config.GetConfig("config", "json", "./config")
+
+	repository := postgres.GetPostgresRepository(configs.DatabaseAddress())
+
+	tokenMaker, _ := token.NewPasetoMaker(configs.TokenSymmetricKey())
+
+	server := api.NewServer(repository, tokenMaker, configs)
+
+	server.Start(configs.ServerAddress())
 }
