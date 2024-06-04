69973d292a2f3f1761efe5ebbd42bb1b4c694bc3
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-06-04 15:00:30 +0330

    updated tests

--- config/config_test.go
+++ config/config_test.go
@@ -11,10 +11,13 @@ func TestGetConfig(t *testing.T) {
 	conf := GetConfig("config_test", "json", ".")
 	require.NotEmpty(t, conf)
 
-	require.Equal(t, "SERVER_ADDRESS", conf.serverAddress)
-	require.Equal(t, "DATABASE_ADDRESS", conf.databaseAddress)
-	require.Equal(t, "TEST_DATABASE_ADDRESS", conf.testDatabaseAddress)
-	require.Equal(t, "TOKEN_SYMMETRIC_KEY", conf.tokenSymmetricKey)
+	require.Equal(t, "0.0.0.0:8080", conf.serverAddress)
+	require.Equal(t, "postgresql://root:1234@localhost:5432/chat_server?sslmode=disable", conf.databaseAddress)
+	require.Equal(t, "postgresql://root:1234@localhost:5432/chat_server_test?sslmode=disable", conf.testDatabaseAddress)
+	require.Equal(t, "********************************", conf.tokenSymmetricKey)
 	require.Equal(t, 15*time.Minute, conf.accessTokenDuration)
 	require.Equal(t, 24*time.Hour, conf.refreshTokenDuration)
+	require.Equal(t, "/api/chat", conf.accessTokenCookiePath)
+	require.Equal(t, "/api/refresh", conf.refreshTokenCookiePath)
+	require.Equal(t, "/chat", conf.usernameCookiePath)
 }
